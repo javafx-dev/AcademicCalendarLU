234d5fdf5f14b0900cee427fb175fabe89965906
Karis Druckenmiller <karisdruckenmiller@live.com>
2017-03-20 21:20:48 -0500

    set application image to the stage

--- src/academiccalendar/ui/main/AcademicCalendar.java
+++ src/academiccalendar/ui/main/AcademicCalendar.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -20,6 +21,10 @@ public class AcademicCalendar extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        stage.getIcons().add(
+        new Image( "academiccalendar/ui/icons/app_icon.png" )); 
+        
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
