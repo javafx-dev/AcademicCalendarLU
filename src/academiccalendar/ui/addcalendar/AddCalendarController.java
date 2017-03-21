93f6f26a26de43a3914f30bf05fa3f4d62f2eb5b
Karis Druckenmiller <karisdruckenmiller@live.com>
2017-03-20 21:15:57 -0500

    new fxml document for the add calendar window

---
+++ src/academiccalendar/ui/addcalendar/AddCalendarController.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package academiccalendar.ui.addcalendar;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Karis
+ */
+public class AddCalendarController implements Initializable {
+
+    @FXML
+    private Label topLabel;
+    @FXML
+    private Label exit;
+    @FXML
+    private JFXTextField calendarName;
+    @FXML
+    private JFXComboBox<String> startMonth;
+    @FXML
+    private JFXComboBox<String> endMonth;
+    @FXML
+    private JFXButton generate;
+    @FXML
+    private JFXButton cancel;
+    
+    // These fields are for mouse dragging of window
+    private double xOffset;
+    private double yOffset;
+    @FXML
+    private AnchorPane rootPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+            ObservableList<String> months = 
+        FXCollections.observableArrayList(
+           "January",
+           "February",
+           "March",
+           "April",
+           "May",
+           "June",
+           "July",
+           "August",
+           "September",
+           "October",
+           "November",
+           "December"
+        );
+            
+        startMonth.setItems(months);
+        endMonth.setItems(months);
+        
+            
+        // Set up Mouse Dragging for the Event pop up window
+        topLabel.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                xOffset = stage.getX() - event.getScreenX();
+                yOffset = stage.getY() - event.getScreenY();
+            }
+        });
+        // Set up Mouse Dragging for the Event pop up window
+        topLabel.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                stage.setX(event.getScreenX() + xOffset);
+                stage.setY(event.getScreenY() + yOffset);
+            }
+        });
+        // Change cursor when hover over draggable area
+        topLabel.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                Scene scene = stage.getScene();
+                scene.setCursor(Cursor.HAND); //Change cursor to hand
+            }
+        });
+        
+        // Change cursor when hover over draggable area
+        topLabel.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                Scene scene = stage.getScene();
+                scene.setCursor(Cursor.DEFAULT); //Change cursor to hand
+            }
+        });
+    }    
+
+    @FXML
+    private void exit(MouseEvent event) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void cancel(MouseEvent event) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
+    }
+    
+}
