1ea16f23e443cdd7861ae2101da1191e9522a8ca
Karis Druckenmiller <karisdruckenmiller@live.com>
2017-03-26 16:45:41 -0500

    function for calculating month labels based on month and year

--- src/academiccalendar/ui/main/FXMLDocumentController.java
+++ src/academiccalendar/ui/main/FXMLDocumentController.java
@@ -5,47 +5,43 @@
  */
 package academiccalendar.ui.main;
 
+import academiccalendar.data.model.Model;
 import com.jfoenix.controls.*;
 import java.io.IOException;
 import java.net.URL;
-import java.time.format.DateTimeFormatter;
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 
 public class FXMLDocumentController implements Initializable {
     
-    // Calendar fields
-    int currentMonth;
-    int currentYear;
-    
-    // The root
+    // Root pane
     @FXML
     private AnchorPane rootPane;
     
-    //Menu
+    // Menu labels
     @FXML
     private Label newCalendarBtn;
     @FXML
@@ -53,82 +49,43 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label helpBtn;
     
-    //  Term Tab Controls
-    @FXML
-    private Pane term_pane;
-    @FXML
-    private JFXDatePicker startDate;
-    @FXML
-    private JFXDatePicker endDate;
-    @FXML
-    private JFXColorPicker eventColor;
-    @FXML
-    private JFXCheckBox endDateCheck; 
-    @FXML
-    private JFXTextField eDescription;
-    
     // Combo/Select Boxes
     @FXML
-    private JFXComboBox<String> termSelect = new JFXComboBox<String>();
+    private JFXComboBox<String> selectedYear;
     @FXML
-    private JFXComboBox<String> yearSelect = new JFXComboBox <String>();
+    private JFXComboBox<String> selectedMonth;
     @FXML
-    private JFXListView<Pane> monthSelect;
+    private JFXListView<String> monthSelect;
     
-    // Main Tab Labels
-    @FXML
-    private Label eEndLabel;
-    @FXML
-    private Label eStartLabel;
-    
-    // Grid
+    // Grid (has all the dates)
     @FXML
     private GridPane calendarGrid;
     
     // Functions
     @FXML
-    void editEvent(MouseEvent event) {
+    private void editEvent(MouseEvent event) {
+        
+        // Get the day that was clicked on
+        Pane p = (Pane) event.getSource();
+        Label lbl = (Label) p.getChildren().get(0);
+        System.out.println(lbl.getText());
+        
+        // Store event day in data singleton
+        Model.getInstance().event_day = Integer.parseInt(lbl.getText());
+        
         // When user clicks on any date in the calendar, event editor window opens
         loadWindow("/academiccalendar/ui/addevent/add_event.fxml", "Event");
     }    
     
     @FXML
-    void newCalendarEvent(MouseEvent event) {
+    private void newCalendarEvent(MouseEvent event) {
         // When the user clicks "New Calendar" pop up window that let's them enter dates
         loadWindow("/academiccalendar/ui/addcalendar/add_calendar.fxml", "Calendar");
-    }
-
-    // Get user input from Date Picker
-    private void getCalenderDates()
-    {
-        // Can make these fields global
-        int startYear = startDate.getValue().getYear();
-        int endYear = endDate.getValue().getYear();
-        
-        int startMonth = startDate.getValue().getMonthValue();
-        int endMonth = endDate.getValue().getMonthValue();
-        
-        int startDay =  startDate.getValue().getDayOfMonth();
-        int endDay =  endDate.getValue().getDayOfMonth();   
-        
-        String test = startDate.getValue().format(DateTimeFormatter.ISO_DATE);
-        System.out.println(test);
-    }
-    
-    private void getEventDescr()
-    {
-        String eDescrip;
-        eDescrip = eDescription.getText();
-    }
-    
-    private boolean isEndDateChecked(boolean checked)
-    {
-        checked = endDateCheck.isSelected();
-        return checked;
     }
     
     private void loadWindow(String loc, String title) {
         try {
+            // Create new stage from FXML given its location
             Parent parent = FXMLLoader.load(getClass().getResource(loc));
             Stage stage = new Stage(StageStyle.UNDECORATED);
             stage.setTitle(title);
@@ -140,67 +97,127 @@ public class FXMLDocumentController implements Initializable {
         
     }
     
-    private void loadMonthSelector(ObservableList<String> months)
+    private void loadMonthSelector()
     {
-        for (int i = 0; i < months.size(); i++) {
-            
-            Label lbl = new Label(months.get(i));
-            StackPane pane = new StackPane();
-            StackPane.setAlignment(pane,Pos.CENTER_LEFT);
-            pane.setPrefSize(50,50); 
-            pane.getChildren().add(lbl);
-            pane.getStyleClass().add("month-select-pane");    
-           
-            monthSelect.getItems().add(pane);
+        // Get a list of all the months in a year
+        DateFormatSymbols dateFormat = new DateFormatSymbols();
+        String months[] = dateFormat.getMonths();
+        
+        // Add all months to the selection combo box
+        selectedMonth.getItems().addAll(months);
+        
+        // Add each to month selector
+        monthSelect.getItems().addAll(months);
+        
+        
+        monthSelect.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                    
+                    // New month has been selected
+                    selectedMonth.getSelectionModel().select(newValue);
+                    
+                    // Change labels based on month selected
+                    loadCalendarLabels(2017, getMonthIndex(newValue));
+                    
+                    
+                }
+            });
+    }
+    
+    private int getMonthIndex(String month){
+        switch (month)
+        {    
+            case "January":
+                return 0;
+            case "February":
+                return 1;
+            case "March":
+                return 2;
+            case "April":
+                return 3;
+            case "May":
+                return 4;
+            case "June":
+                return 5;
+            case "July":
+                return 6;
+            case "August":
+                return 7;
+            case "September":
+                return 8;
+            case "October":
+                return 9;
+            case "November":
+                return 10;
+            case "December":
+                return 11;
         }
+        return 0;
+    }
+    
+    private void loadCalendarLabels(int year, int month){
+        
+        // Based on month and year, get the start day week
+        // and the number of days in a month
+        GregorianCalendar gc = new GregorianCalendar(year, month, 1);
+        int firstDay = gc.get(Calendar.DAY_OF_WEEK);
+        int daysInMonth = gc.getActualMaximum(Calendar.DAY_OF_MONTH);
+        System.out.println("First day: " + firstDay + " Days in Month: " + daysInMonth);
+        
+        // Start at first day
+        int labelCount = 1;
+        
+        // 7 days of the week
+        int numWeekDays = 7;
+        int dayCount = 1;
+        
+        int offset = numWeekDays + firstDay;
+      
+        // Remove old labels
+        for (Node node: calendarGrid.getChildren()) {
+            Pane p = (Pane) node;
+            
+            if (dayCount <= numWeekDays) {
+               dayCount++;
+            } else {
+                p.getChildren().clear();
+            }
+        }
+       
+        dayCount = 1;
+        
+       // Go through calendar grids
+       for(Node node : calendarGrid.getChildren()){
+           
+           Pane p = (Pane) node;
+           
+           // Start placing labels on the first day for the month
+           if (dayCount < offset) {
+               dayCount++;
+           } else {
+               
+            // Make new day label   
+            Label lbl = new Label(Integer.toString(labelCount));
+            lbl.setPadding(new Insets(5));
+            lbl.setStyle("-fx-text-fill:darkslategray");
+
+            p.getChildren().add(lbl);
+
+            // Maximum days reached for that month
+            if (labelCount == daysInMonth) { System.out.print("Reached max for the month."); break;}
+            labelCount++;           
+           }
+           
+       }
     }
     
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb) { 
         
-        ObservableList<String> months = 
-    FXCollections.observableArrayList(
-       "January",
-       "February",
-       "March",
-       "April",
-       "May",
-       "June",
-       "July",
-       "August",
-       "September",
-       "October",
-       "November",
-       "December"
-    );
+    loadMonthSelector();
     
-          ObservableList<String> terms = 
-    FXCollections.observableArrayList(
-       "MBA SEM",
-       "Undergraduate SEM",
-       "Quarter SEM",
-       "Half SEM"      
-    );
-      
-    termSelect.setItems(terms);
-    
-      ObservableList<String> years = 
-    FXCollections.observableArrayList(
-       "2017",
-       "2018",
-       "2019",
-       "2020",
-       "2021",
-       "2022",
-       "2023",
-       "2024",
-       "2025"
-            
-    );
-      
-    yearSelect.setItems(years);
-    
-    loadMonthSelector(months);
+    // ******** Everything below here is for Draggable Windows *******
     
       // Change cursor when hover over draggable area
         newCalendarBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
