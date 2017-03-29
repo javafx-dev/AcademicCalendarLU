964b5092ffc68830189a06cf10d10132bf704337
Karis Druckenmiller <karisdruckenmiller@live.com>
2017-03-29 14:35:36 -0500

    calling maximize window function (commented out for now)

--- src/academiccalendar/ui/main/AcademicCalendar.java
+++ src/academiccalendar/ui/main/AcademicCalendar.java
@@ -26,6 +26,9 @@ public class AcademicCalendar extends Application {
         stage.getIcons().add(
         new Image( "academiccalendar/ui/icons/app_icon.png" ));
         
+        // Maximize window at launch
+        //stage.setMaximized(true);
+        
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
