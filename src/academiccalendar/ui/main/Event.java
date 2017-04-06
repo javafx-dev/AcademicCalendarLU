2d462dff714cf439d805d7d338d4872d59f8352e
Karis Druckenmiller <karisdruckenmiller@live.com>
2017-04-05 20:36:16 -0500

    added getters for properties

--- src/academiccalendar/ui/main/Event.java
+++ src/academiccalendar/ui/main/Event.java
@@ -11,14 +11,27 @@ import javafx.beans.property.SimpleStringProperty;
  *
  * @author Paul
  */
-public  class Event {
-     private final SimpleStringProperty term;
+public class Event {
+    private final SimpleStringProperty term;
     private final SimpleStringProperty subject;
     private final SimpleStringProperty date;
  
-    Event(String term, String subject, String date) {
+    public Event(String term, String subject, String date) {
         this.term = new SimpleStringProperty(term);
         this.subject = new SimpleStringProperty(subject);
         this.date = new SimpleStringProperty(date);
+    }
+    
+    // Note: We need these accessors.
+    public String getTerm() {
+        return term.get();
+    }
+
+    public String getSubject() {
+        return subject.get();
+    }
+
+    public String getDate() {
+        return date.get();
     }
 }
