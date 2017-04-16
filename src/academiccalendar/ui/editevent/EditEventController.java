0199adc0f11458eec7da08824228b0e7e2e1c302
Karis Druckenmiller <karisdruckenmiller@live.com>
2017-04-16 10:31:47 -0500

    set up auto fill for edit window, draggable window, and fxml fields

--- src/academiccalendar/ui/editevent/EditEventController.java
+++ src/academiccalendar/ui/editevent/EditEventController.java
@@ -5,17 +5,24 @@
  */
 package academiccalendar.ui.editevent;
 
+import academiccalendar.data.model.Model;
 import academiccalendar.database.DBHandler;
 import academiccalendar.ui.main.FXMLDocumentController;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -40,21 +47,86 @@ public class EditEventController implements Initializable {
     private JFXComboBox<String> termSelect;
     @FXML
     private JFXDatePicker date;
+    @FXML
+    private AnchorPane rootPane;
     
     public void setMainController(FXMLDocumentController mainController) {
         this.mainController = mainController ;
     }
 
+    // These fields are for mouse dragging of window
+    private double xOffset;
+    private double yOffset;
+    
+    void autofillDatePicker() {
+       // Get selected day, month, and year and autofill date selection
+       int day = Model.getInstance().event_day;
+       int month = Model.getInstance().event_month + 1;
+       int year = Model.getInstance().event_year;
+       
+       String descript = Model.getInstance().event_subject;
+       
+       // Set default value for datepicker
+       date.setValue(LocalDate.of(year, month, day));
+       
+       // Fill description field
+       subject.setText(descript);
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        autofillDatePicker();
+        
+        // ************* Everything below is for Draggable Window ********
+        
+        // Set up Mouse Dragging for the Event pop up window
+        topLabel.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                xOffset = stage.getX() - event.getScreenX();
+                yOffset = stage.getY() - event.getScreenY();
+            }
+        });
+        // Set up Mouse Dragging for the Event pop up window
+        topLabel.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                stage.setX(event.getScreenX() + xOffset);
+                stage.setY(event.getScreenY() + yOffset);
+            }
+        });
+        // Change cursor when hover over draggable area
+        topLabel.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                Scene scene = stage.getScene();
+                scene.setCursor(Cursor.HAND); //Change cursor to hand
+            }
+        });
+        
+        // Change cursor when hover over draggable area
+        topLabel.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                Scene scene = stage.getScene();
+                scene.setCursor(Cursor.DEFAULT); //Change cursor to hand
+            }
+        });
     }    
 
     @FXML
     private void exit(MouseEvent event) {
+        // Close the window
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
